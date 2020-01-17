9c570a356907095f257b7e1b0f3e6881ffa25225
Rahul <rahulchopra.rc93@gmail.com>
2020-01-17 14:25:15 +0530

    correction

--- Backend/data/dbConfig.js
+++ Backend/data/dbConfig.js
@@ -1,19 +1,28 @@
-const mongoose = require("mongoose");
-// var dburl 	 = 'mongodb://@localhost:27017/chatapp';
-var dburl 	 = 'mongodb+srv://NitinKumar:mongo@Nitin@minicluster-rr1b0.mongodb.net/test?retryWrites=true&w=majority';
+// const mongoose = require("mongoose");
+// // var dburl 	 = 'mongodb://@localhost:27017/chatapp';
+// var dburl 	 = 'mongodb+srv://NitinKumar:mongo@Nitin@minicluster-rr1b0.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.Promise = global.Promise;
-//mongoose.Promise = require('bluebird');
-mongoose.connect(dburl,{ useUnifiedTopology: true,useNewUrlParser: true});
+// mongoose.Promise = global.Promise;
+// //mongoose.Promise = require('bluebird');
+// mongoose.connect(dburl,{ useUnifiedTopology: true,useNewUrlParser: true,useCreateIndex : true});
 
-mongoose.connection.on('connected', function() {
-	console.log("mongoose connected to "+ dburl);
-});
+// mongoose.connection.on('connected', function() {
+// 	console.log("mongoose connected to "+ dburl);
+// });
 
-mongoose.connection.on('disconnected', function(err) {
-	console.log("mongoose disconnected"+ err);
-});
+// mongoose.connection.on('disconnected', function(err) {
+// 	console.log("mongoose disconnected"+ err);
+// });
 
-mongoose.connection.on('error', function(err) {
-	console.log("mongoose connection error: " + err);
+// mongoose.connection.on('error', function(err) {
+// 	console.log("mongoose connection error: " + err);
+// });
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://NitinKumar:mongo@Nitin@minicluster-rr1b0.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
