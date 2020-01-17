243591d1fb9c3636dfe6f2d35bf60ec5ad445996
Rahul <rahulchopra.rc93@gmail.com>
2020-01-17 12:54:50 +0530

    correction

--- Backend/data/dbConfig.js
+++ Backend/data/dbConfig.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-var dburl 	 = 'mongodb://@localhost:27017/chatapp';
+// var dburl 	 = 'mongodb://@localhost:27017/chatapp';
+var dburl 	 = 'mongodb+srv://NitinKumar:mongo@Nitin@minicluster-rr1b0.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.Promise = global.Promise;
 //mongoose.Promise = require('bluebird');
