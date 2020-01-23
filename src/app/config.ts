7b1497b11165518e078454ffafead58d5c2933fb
Rahul <rahulchopra.rc93@gmail.com>
2020-01-23 11:23:40 +0530

    corect

---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+export class CONFIG {
+    public static KEY = {
+        CLIENTID :  '36814645377-rkvufq4l6j48rm8oaoriae1408137mtn.apps.googleusercontent.com',
+        GOOGLE_MAP_KEY : 'AIzaSyB_G7ern6dzWvSUGnVtT2shOG98xXqpZbQ',
+        CAPTCHA_SECRET_KEY : '6Lc999EUAAAAALc5chiKMwfBhpVlDNXRcsk_YR77',
+        CAPTCHA_SITE_KEY : '6Lc999EUAAAAAFkBL3KmtXZU8DYD-CW4qpxCoQvG'
+    }
+}
